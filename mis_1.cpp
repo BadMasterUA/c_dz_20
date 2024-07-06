c534789a41695c5f2ffcdd55b6c5a66cc158b617
DGHaZe <34streets@gmail.com>
2024-07-06 08:53:38 +0300

    final

--- mis_1.cpp
+++ mis_1.cpp
@@ -2,16 +2,12 @@
 #include <fstream>
 
 using namespace std;
-/*
-bool is_printable(char ch)
-{
-    return ch >= 32 && ch <= 126 && ch != 9;
-}*/
 
 int main(int argc, char *argv[])
 {
-    char *fileName = argv[1]; // берем содержимое второго аргумента в качестве имени файла
+    char* fileName = argv[1]; // берем содержимое второго аргумента в качестве имени файла
     ifstream fin(fileName);
+    //проверка на наличие файла в каталоге
     if (!fin)
     {
         cout << "Unable to open " << fileName << " for reading.\n";
@@ -21,7 +17,7 @@ int main(int argc, char *argv[])
     char ch;
     while (fin.get(ch))
     {
-        if (ch != 10 && ch != 9) // 32 126 9 ASCII
+        if (ch != '\n' && ch != '\r' && ch != '\t') // 32 126 9 ASCII 10 9
             cout << ch;
     }
     fin.close(); // закрываем файл
